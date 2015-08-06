92c363156320ac7abce888cffd21e08f4815fa4b
nahc-ak <6lo.olg@gmail.com>
2015-08-06 20:12:54 +0900

    first commit

---
+++ NIM2015/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NIM2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+
+            do
+            {
+                Console.Write("尻を 叩いて 下さい: ");
+                str = Console.ReadLine();
+                if (str.Equals("ッパーン"))
+                {
+                    Console.WriteLine("ほ～い");
+                }
+            }
+            while (!str.Equals("exit"));
+        }
+    }
+}
